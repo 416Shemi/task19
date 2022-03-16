e638c1f5ebfc035f10e713958f229c092a0aea65
416Shemi <tu7ngcsvr@code.edu.az>
2022-03-16 11:03:53 +0400

    new projcet19

---
+++ ConsoleApp19/Models/Vepon.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19.Models
+{
+    class Vepon
+    {
+
+        private int _BulledCapacity;
+        private int _NumberBulletsin;
+        private int _SecondsCombing;
+        private string _AtisMode;
+        public int BulledCapacity
+        {
+            get
+            {
+                return _BulledCapacity;
+            }
+            set
+            {
+                if(value>0 && value < 50)
+                {
+                    _BulledCapacity = value;
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Uygun deyil");
+                }
+            }
+        }
+        public int NumberBulletsin
+        {
+            get
+            {
+                return _NumberBulletsin;
+            }
+            set
+            {
+                if(value>3 && value < 20)
+                {
+                    _NumberBulletsin = value;
+                }
+                else
+                {
+                    Console.WriteLine("Duzgun daxl edin");
+                }
+            }
+        }
+        public int SecondsCombing
+        {
+            get
+            {
+                return _SecondsCombing;
+            }
+            set
+            {
+                if(value>0 && value < 5)
+                {
+                    _SecondsCombing = value;
+
+                }
+                else
+                {
+                    Console.WriteLine("Duzgun daxil edin");
+                }
+            }
+        }
+        public string AtisMode
+        {
+            get
+            {
+                return _AtisMode;
+            }
+            
+        }
+        public Vepon(int BulledCapacity, int NumberBulletsin, int SecondsCombing, string AtisMode)
+        {
+            this._BulledCapacity = BulledCapacity;
+            this._NumberBulletsin = NumberBulletsin;
+            this._SecondsCombing = SecondsCombing;
+            this._AtisMode = AtisMode;
+        }
+        public void Shoot()
+        {
+            int k = 0;
+            if (k > 0)
+            {
+                k--;
+                Console.WriteLine("A projectile was fired");
+            }
+            else
+            {
+                Console.WriteLine("There are no shells");
+            }
+        }
+        public void Fire()
+        {
+            
+            {
+                int cont=0;
+                if (cont == 0)
+                {
+                    cont = _SecondsCombing;
+                }
+
+            }
+            
+
+        }
+        public void GetRemainBulletCount()
+        {
+
+            if (_NumberBulletsin < _BulledCapacity)
+            {
+                Console.WriteLine(_BulledCapacity - _NumberBulletsin);
+
+            }
+            else
+            {
+                Console.WriteLine("The comb is full");
+            }
+
+        }
+        public void Reload()
+        {
+            if (_NumberBulletsin < _BulledCapacity)
+            {
+
+                _NumberBulletsin = _BulledCapacity;
+                Console.WriteLine("The comb is filled");
+            }
+
+            else
+            {
+                Console.WriteLine("The comb is filled");
+            }
+
+        }
+        public void ChangeFireMode()
+        {
+            if (_AtisMode == "true")
+            {
+                Console.WriteLine("Shooting mode is single");
+            }
+            else if (_AtisMode == "false")
+            {
+                Console.WriteLine("Shooting mode is automatic");
+            }
+        }
+    }
+}
---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp19.Models;
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Vepon gun = new Vepon(30, 10, 2, "false");
+
+            int key = 0;
+            while (key != 6)
+            {
+                Console.WriteLine("key");
+                key = Convert.ToInt32(Console.ReadLine());
+                switch (key)
+                {
+                    case 0:
+                        {
+                            gun.Shoot();
+                        }
+                        break;
+                    case 1:
+                        {
+                            gun.Fire();
+                        }
+                        break;
+                    case 2:
+                        {
+                            gun.GetRemainBulletCount();
+                        }
+                        break;
+                    case 3:
+                        {
+                            gun.Reload();
+                        }
+                        break;
+                    case 4:
+                        {
+                            gun.ChangeFireMode();
+                        }
+                        break;
+
+                    default:
+                        {
+                            Console.WriteLine("this is wrong");
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
